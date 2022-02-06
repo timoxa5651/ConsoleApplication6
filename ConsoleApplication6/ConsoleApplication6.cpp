6d7b003d52d769f8f1e17dfc60210fd7acc83875
PC <timoxa5651@gmail.com>
2022-02-06 15:18:12 +0300

    1

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -17,10 +17,12 @@ template<typename T = int>
 struct Node {
 public:
 	Node<T>* next;
+	Node<T>* prev;
 	T data;
 
-	Node(T& dt, Node<T>* nxt = nullptr) {
+	Node(T& dt, Node<T>* nxt = nullptr, Node<T>* prv = nullptr) {
 		this->next = nxt;
+		this->prev = prv;
 		this->data = dt;
 	}
 
@@ -35,88 +37,54 @@ class List {
 	iterator head;
 
 	template<typename _Pred>
-	iterator MakePartition(iterator head, iterator end, _Pred pred, iterator* newHead, iterator* newMid, iterator* newEnd) {
-		iterator pivot = end;
-		iterator prev = nullptr, cur = head, tail = pivot;
-		*newMid = tail;
-		while (cur != end) {
-			if (cur->data == pivot->data) {
-				if (prev)
-					prev->next = cur->next;
-				iterator tmp = cur->next;
-				cur->next = (*newMid)->next;
-				(*newMid)->next = cur;
-				if (tail == *newMid)
-					tail = cur;
-				*newMid = cur;
-				cur = tmp;
-			}
-			else if (pred(cur->data, pivot->data)) {
-				if (prev)
-					prev->next = cur->next;
-				iterator tmp = cur->next;
-				cur->next = nullptr;
-				tail->next = cur;
-				tail = cur;
-				cur = tmp;
-			}
-			else
-			{
-				if ((*newHead) == nullptr)
-					(*newHead) = cur;
-				prev = cur;
-				cur = cur->next;
-			}
-		}
-		if ((*newHead) == nullptr) {
-			(*newHead) = pivot;
-		}
-		(*newEnd) = tail;
-		return pivot;
-	}
-
-	template<typename _Pred>
-	iterator Sort(iterator start, iterator end, _Pred pred) {
-		if (!start || !end || start == end)
+	iterator Sort(iterator start, _Pred pred) {
+		if (!start || !start->next)
 			return start;
-		iterator newHead = nullptr, newEnd = nullptr, newMid = nullptr;
-		iterator pivot = this->MakePartition(start, end, pred, &newHead, &newMid, &newEnd);
+		iterator right = start;
+		iterator temp = start;
+		while (temp && temp->next)
+		{
+			right = right->next;
+			temp = temp->next->next;
+		}
 
-		/*iterator t1 = newHead;
-		while (t1 != pivot) {
-			cout << t1->data << " ";
-			t1 = t1->next;
-		}
-		cout << "ddd ";
-		while (t1 != newMid) {
-			cout << t1->data << " ";
-			t1 = t1->next;
-		}
-		cout << "eee ";
-		while (t1 != newEnd) {
-			cout << t1->data << " ";
-			t1 = t1->next;
-		}
-		cout << "\n";*/
+		if (right->prev)
+			right->prev->next = nullptr; // cut in half
 
-		if (newHead != pivot) { // head -> pivot
-			iterator tmp = newHead;
-			while (tmp->next != pivot)
-				tmp = tmp->next;
-			tmp->next = nullptr;
-			newHead = this->Sort(newHead, tmp, pred);
-			tmp = newHead;
-			while (tmp->next) {
-				tmp = tmp->next;
+		iterator list = this->Sort(start, pred);
+		right = this->Sort(right, pred);
+
+		iterator next = nullptr;
+		iterator result = nullptr;
+		iterator tail = nullptr;
+		while (list || right)
+		{
+			if (!right) {
+				next = list;
+				list = list->next;
 			}
-			tmp->next = pivot;
+			else if (!list) {
+				next = right;
+				right = right->next;
+			}
+			else if (!pred(list->data, right->data)) {
+				next = list;
+				list = list->next;
+			}
+			else {
+				next = right;
+				right = right->next;
+			}
+			if (!result) {
+				result = next;
+			}
+			else {
+				tail->next = next;
+			}
+			next->prev = tail;
+			tail = next;
 		}
-		// (pivot, mid] - same elements, skip
-		// mid -> end
-		if(newMid->next) //not end
-			newMid->next = this->Sort(newMid->next, newEnd, pred);
-
-		return newHead;
+		return result;
 	}
 public:
 	size_t size;
@@ -141,7 +109,9 @@ public:
 	}
 
 	iterator InsertFirst(T data) {
-		this->head = new Node(data, this->head);
+		this->head = new Node<T>(data, this->head, nullptr);
+		if (this->head->next)
+			this->head->next->prev = this->head;
 		this->size += 1;
 		return this->head;
 	}
@@ -149,7 +119,10 @@ public:
 	iterator InsertAfter(iterator element, T data) {
 		if (!element)
 			return nullptr;
-		element->next = new Node(data, element->next);
+		
+		element->next = new Node(data, element->next, element);
+		if (element->next->next)
+			element->next->next->prev = element->next;
 		this->size += 1;
 		return element->next;
 	}
@@ -157,31 +130,28 @@ public:
 	iterator InsertBefore(iterator element, T data) {
 		if (!element)
 			return nullptr;
-		iterator anext = new Node(element->data, element->next);
-		element->data = data;
-		element->next = anext;
-		this->size += 1;
-		return element;
+		if (element->prev)
+			return this->InsertAfter(element->prev, data);
+		return this->InsertFirst(data);
 	}
 
 	void Delete(iterator iter) {
-		if (!iter || !iter->next)
+		if (!iter)
 			return;
-		iterator q = iter->next;
-		iter->data = iter->next->data;
-		iter->next = iter->next->next;
-		delete q;
+		if (iter->prev)
+			iter->prev->next = iter->next;
+		else
+			this->head = iter->next;
+
+		if (iter->next)
+			iter->next->prev = iter->prev;
+		delete iter;
+		this->size -= 1;
 	}
 
 	template<typename _Pred>
 	void Sort(_Pred pred) {
-		if (!this->head)
-			return;
-		iterator tail = this->head;
-		while (tail->next) {
-			tail = tail->next;
-		}
-		this->head = this->Sort(this->head, tail, pred);
+		this->head = this->Sort(this->head, pred);
 	}
 
 	void Sort() {
@@ -198,7 +168,9 @@ int main()
 	//list->InsertBefore(list->InsertFirst(9), 5);
 	
 	for (int i = 1; i <= 10; ++i)
-		list->InsertFirst(i);
+		list->InsertFirst(rand());
+	//list->InsertBefore(list->InsertFirst(7), 9);
+	list->Delete(list->InsertAfter(list->InsertFirst(5), 8));
 
 	for (auto it = list->begin(); it != list->end(); it = it->walk()) {
 		cout << it->data << " ";
