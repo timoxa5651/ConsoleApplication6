fc5e1404f0bdc2a1c26ca8da11fdd62904f183e7
PC <timoxa5651@gmail.com>
2022-04-28 21:04:10 +0300

    1

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -144,7 +144,7 @@ public:
 		return rstr;
 	}
 
-	int get_num(int st, bool read, bool negat = false, bool db = false) {
+	double get_num(int st, bool read, bool negat = false, bool db = false) {
 		bool ng = this->str[st] == '-';
 		if (!negat && ng)
 			throw ReadExpection(this->off, "Only positive numbers are allowed");
@@ -154,7 +154,8 @@ public:
 				this->off += 1;
 		}
 
-		int rd = 0, rs = 0;
+		double rs = 0;
+		int rd = 0;
 		for (int i = st; i < this->str.getSize(); ++i) {
 			if (this->str[i] >= '0' && this->str[i] <= '9') {
 				rd = max(1, rd * 10);
@@ -171,6 +172,15 @@ public:
 			if (read) {
 				this->off += 1;
 			}
+			st += 1;
+		}
+
+		if (db && this->peek_char() == L'.') {
+			if (read)
+				this->seek(1);
+			double pst = this->get_num(st + 1, read, false, false);
+			while (pst > 1.0) pst /= 10;
+			rs += pst;
 		}
 		return rs * (ng ? -1 : 1);
 	}
@@ -180,7 +190,15 @@ public:
 enum class OpType {
 	ConstNumber,
 	DynNumber,
-	Plus
+
+	Plus,
+	Minus,
+	Multiply,
+	Divide,
+	Pow,
+
+	Cos,
+	Sin
 };
 class Opdef {
 public:
@@ -191,32 +209,56 @@ public:
 	int priority;
 
 	template<typename F>
-	Opdef(OpType type, string repr, F&& call, bool isf, int pr = 0) {
+	Opdef(OpType type, string repr, const F& call, bool isf, int pr = 0) {
 		this->repr = repr;
 		this->eval = call;
 		this->type = type;
 		this->isFunction = isf;
+		this->priority = pr;
+	}
+
+	int GetArgCount() {
+		if (this->isFunction)
+			return 1;
+		switch (this->type) {
+		case OpType::ConstNumber:
+		case OpType::DynNumber:
+			return 0;
+		}
+		return 2;
 	}
 };
 static map<OpType, Opdef*> g_opcodes;
 
-#define REG_OPCODE(name, repr, pr, fun) g_opcodes[OpType::name] = new Opdef(OpType::name, repr, fun, pr, false);
-#define REG_FUNC(name, repr, fun) g_opcodes[OpType::name] = new Opdef(OpType::name, repr, fun, true);
+#define REG_OPCODE(name, repr, pr, fun) g_opcodes[OpType::name] = new Opdef(OpType::name, repr, fun, false, pr)
+#define REG_FUNC(name, repr, fun) g_opcodes[OpType::name] = new Opdef(OpType::name, repr, fun, true)
 
 typedef struct OpNode {
-	OpNode* left, *right;
-	OpType type;
+	OpNode* left, * right;
+	Opdef* def;
 	double arg;
 
-	OpNode() {
+	OpNode(OpType type) {
 		this->left = this->right = nullptr;
+		this->arg = 0;
+		this->def = g_opcodes[type];
 	}
-} Node, *PNode;
+	~OpNode() {
+		if (this->left) {
+			delete this->left;
+			this->left = 0;
+		}
+		if (this->right) {
+			delete this->right;
+			this->right = 0;
+		}
+	}
+} Node, * PNode;
 
 Opdef* FindOpdef(string str) {
 	for (auto& [type, def] : g_opcodes) {
 		string& dd = def->repr;
-		if (str.starts_with(dd))
+		if (dd.length() && str.starts_with(dd))
 			return def;
 	}
 	return nullptr;
@@ -228,44 +270,39 @@ PNode ExprParse(Stream stream) {
 	std::stack<pair<string, Opdef*>> funcs;
 	while (stream.get_cur() < stream.get_size()) {
 		stream.read_while(' ');
-
 		try {
-			double num = stream.get_num(stream.get_cur(), true, true, true);
-			PNode node = new Node();
-			node->type = OpType::ConstNumber;
+			double num = stream.get_num(stream.get_cur(), true, false, true);
+			PNode node = new Node(OpType::ConstNumber);
 			node->arg = num;
-			nodes.emplace_back(node);
+			nodes.push_back(node);
 			continue;
 		}
 		catch (ReadExpection& ex) {}
 
 		if (stream.peek_char() == '(') {
 			funcs.push(make_pair("(", nullptr));
+			stream.seek(1);
 			continue;
 		}
 		else if (stream.peek_char() == ')') {
-			vector<PNode> args;
+			stream.seek(1);
 			while (funcs.size()) {
 				auto [r, fun] = funcs.top();
 				if (r == '(') {
 					funcs.pop();
 					break;
 				}
-				args.emplace_back(fun);
+				PNode node = new Node(fun->type);
+				nodes.push_back(node);
 				funcs.pop();
 			}
 
 			if (funcs.size() && funcs.top().second && funcs.top().second->isFunction) {
 				auto func = funcs.top().second;
-				PNode node = new Node();
-				node->type = func->type;
-				node->left = args[0];
+				PNode node = new Node(func->type);
 				nodes.push_back(node);
 
 				funcs.pop();
-			}
-			else {
-
 			}
 
 			continue;
@@ -276,145 +313,222 @@ PNode ExprParse(Stream stream) {
 		if (!def) {
 			throw ReadExpection(stream.get_cur(), "No operand");
 		}
+		stream.seek(def->repr.length());
 
-		if (def->isFunction) {
+		if (def->type == OpType::DynNumber) {
+			PNode node = new Node(OpType::DynNumber);
+			nodes.push_back(node);
+		}
+		else if (def->isFunction) {
 			funcs.push(make_pair(def->repr, def));
 		}
 		else {
 			while (funcs.size()) {
 				auto [r, fun] = funcs.top();
-				if (!fun || fun->priority <= def->priority)
+				if (!fun || fun->priority < def->priority)
 					break;
-				nodes.emplace_back(fun);
+				PNode node = new Node(fun->type);
+				nodes.push_back(node);
+
 				funcs.pop();
 			}
 			funcs.push(make_pair(def->repr, def));
 		}
 	}
-	
 
-	return nullptr;
+	while (funcs.size()) {
+		auto [r, fun] = funcs.top();
+		if(!fun)
+			throw ReadExpection(stream.get_cur(), "strike");
+		PNode node = new Node(fun->type);
+		nodes.push_back(node);
+
+		funcs.pop();
+	}
+
+	static PNode(*BuildNode)(vector<PNode>&) = [](vector<PNode>& nodes) -> PNode {
+		if (!nodes.size())
+			return nullptr; // throw
+		PNode curNode = nodes[nodes.size() - 1];
+		nodes.pop_back();
+		int argc = curNode->def->GetArgCount();
+		if (argc > 0) {
+			curNode->right = BuildNode(nodes);
+		}
+		if (argc > 1) {
+			curNode->left = BuildNode(nodes);
+		}
+		return curNode;
+	};
+
+	return BuildNode(nodes);
 }
+
+static double g_GetDynVariable; // :(
 
 double EvalNode(PNode pnode) {
-	return g_opcodes[pnode->type]->eval(pnode);
+	if (!pnode)
+		return 0.0;
+	return pnode->def->eval(pnode);
 }
+
+class Input {
+	Vector2i pos;
+	Vector2i size;
+	String value;
+	bool isActive;
+public:
+	PNode rootNode;
+	void set_active(bool f);
+	Input(Vector2f ps);
+	void render(class WndClass* wndcl);
+	void rebuild();
+	void text_entered(Event evnt);
+	void on_mousedown(Vector2f pos);
+};
 
 class WndClass {
 	RenderWindow* wnd;
-	Vector2f size;
+	Vector2f sizeV;
 
-	String current_text;
-	bool button_state;
+	vector<Input*> inputs;
 
+	friend class Input;
 public:
+	float current_scale;
+	float target_scale;
+	Vector2f offset;
+	float size;
+
+	void create_inputs() {
+		inputs.push_back(new Input(Vector2f(20, 20)));
+		inputs.push_back(new Input(Vector2f(20, 50)));
+		inputs.push_back(new Input(Vector2f(20, 80)));
+
+	}
+
 	WndClass(RenderWindow* wnd, Vector2f size) {
 		this->wnd = wnd;
-		this->size = size;
-		this->button_state = 0;
+		this->sizeV = size;
+		this->size = wnd->getSize().x;
+		this->current_scale = this->size / 6.0;
+		this->current_scale /= 2;
+		this->target_scale = this->current_scale;
+		this->create_inputs();
 	}
 
 	void do_buttons() {
-		Vector2f button_pos = Vector2f(250, 150);
-		Vector2f button_size = Vector2f(25, 25);
+		for (Input* in : this->inputs)
+			in->render(this);
+	}
 
-		Text text;
-		text.setFont(g_Font);
-		text.setCharacterSize(18);
-		text.setFillColor(Color(255, 255, 255, 255));
-		text.setString("Disabled = 1st, Enabled = 2nd");
-		text.setPosition(button_pos + Vector2f(button_size.x * 3, 0));
-		this->wnd->draw(text);
+	void draw_grid() {
 
-		RectangleShape rect(button_size);
-		rect.setPosition(button_pos);
-		rect.setOutlineColor(Color(0, 0, 0, 255));
-		rect.setOutlineThickness(2.f);
-		rect.setFillColor(Color(90, 90, 90, 255));
-		this->wnd->draw(rect);
-		if (this->button_state) {
-			RectangleShape rect(button_size / 2.f);
-			rect.setPosition(button_pos + button_size / 4.f);
-			rect.setFillColor(Color(0, 0, 0, 255));
-			this->wnd->draw(rect);
+		// x axis
+		double xStart = wnd_to_plot(Vector2f(0, 0)).x;
+		double xEnd = wnd_to_plot(Vector2f(this->size, 0)).x;
+		if (xEnd < xStart)
+			swap(xStart, xEnd);
+		xStart = round(xStart);
+		xEnd = round(xEnd);
+		for (double cur = xStart; cur <= xEnd; cur += 1.f) {
+			Vector2f wPos = plt_to_wnd(Vector2f(cur, 0));
+
+			Vertex vertices[2];
+			vertices[0] = Vertex(Vector2f(wPos.x, 0));
+			vertices[1] = Vertex(Vector2f(wPos.x, this->size));
+			vertices[0].color = Color(255, 255, 255, 30);
+			vertices[1].color = Color(255, 255, 255, 30);
+			wnd->draw(vertices, 2, Lines);
+		}
+
+		//y axis
+		xStart = wnd_to_plot(Vector2f(0, 0)).y;
+		xEnd = wnd_to_plot(Vector2f(0, this->size)).y;
+		if (xEnd < xStart)
+			swap(xStart, xEnd);
+		xStart = round(xStart);
+		xEnd = round(xEnd);
+		for (double cur = xStart; cur <= xEnd; cur += 1.f) {
+			Vector2f wPos = plt_to_wnd(Vector2f(0, cur));
+
+			Vertex vertices[2];
+			vertices[0] = Vertex(Vector2f(0, wPos.y));
+			vertices[1] = Vertex(Vector2f(this->size, wPos.y));
+			vertices[0].color = Color(255, 255, 255, 30);
+			vertices[1].color = Color(255, 255, 255, 30);
+			wnd->draw(vertices, 2, Lines);
+		}
+
+		//0, 0
+		Vector2f pPos = plt_to_wnd(Vector2f(0, 0));
+		if (pPos.x >= 0 && pPos.x < this->size && pPos.y >= 0 && pPos.y < this->size) {
+			Text text;
+			text.setFont(g_Font);
+			text.setString("(0,0)");
+			text.setCharacterSize(10);
+			text.setPosition(pPos);
+			wnd->draw(text);
+		}
+
+	}
+
+	Vector2f wnd_to_plot(Vector2f wnd) {
+		wnd += offset;
+		Vector2f rs = (wnd - Vector2f(size, size) / 2.f) / this->current_scale;
+		rs.y = -rs.y;
+		return rs;
+	}
+	Vector2f plt_to_wnd(Vector2f plt) {
+		plt.y = -plt.y;
+		Vector2f rs = plt * this->current_scale + Vector2f(size, size) / 2.f;
+		rs -= Vector2f(offset.x, offset.y);
+		return rs;
+	}
+
+	void render() {
+		double outline_color[] = { 1.f,1.f,1.f,1.f };
+		//outline
+		vector<Vertex> vt_buffer;
+		vt_buffer.reserve(this->wnd->getSize().x);
+		for (Input* sh : this->inputs) {
+			if (!sh->rootNode)
+				continue;
+			for (int x = 0; x <= this->wnd->getSize().x; ++x) {
+				double px = wnd_to_plot(Vector2f(x, 0)).x;
+				// for x 
+				g_GetDynVariable = px;
+				double solution = EvalNode(sh->rootNode);
+				Vector2f spos = plt_to_wnd(Vector2f(px, solution));
+
+				//if (spos.x >= 0 && spos.x < pl->size && spos.y >= 0 && spos.y < pl->size) {
+				Vertex vertex;
+				vertex.position = Vector2f(spos.x, spos.y);
+				vertex.color = Color(outline_color[0] * 255.f, outline_color[1] * 255.f, outline_color[2] * 255.f, outline_color[3] * 255.f);
+				vt_buffer.push_back(vertex);
+				//}
+
+			}
+
+			if (vt_buffer.size())
+				this->wnd->draw(vt_buffer.data(), vt_buffer.size(), PrimitiveType::LinesStrip);
+
+			vt_buffer.clear();
 		}
 	}
 
 	void frame() {
+		this->current_scale += (this->target_scale - this->current_scale) * min(0.75f, abs(this->current_scale - this->target_scale));
+		this->draw_grid();
+		this->render();
+
 		this->do_buttons();
-
-		Vector2f field_size = Vector2f(500, 400);
-		Vector2f field_pos = Vector2f(this->size.x / 2 - field_size.x / 2, this->size.y / 2 - field_size.y / 2);
-
-		RectangleShape rect(field_size);
-		rect.setPosition(field_pos);
-		rect.setOutlineColor(Color(0, 0, 0, 255));
-		rect.setOutlineThickness(1.f);
-		rect.setFillColor(Color(90, 90, 90, 255));
-		this->wnd->draw(rect);
-
-		Text text;
-		text.setFont(g_Font);
-		text.setCharacterSize(20);
-		text.setFillColor(Color(200, 200, 200, 255));
-		text.setString("[None]");
-		text.setPosition(Vector2f(250 + 60, 150 + 400 + 70));
-
-		//cout << "Testing: " << current_text.toAnsiString() << endl;
-		ReadExpection expection = ReadExpection(0, "");
-		bool isErrored = false;
-		if (!this->button_state) {
-			
-			text.setString("Fail: Generic");
-			
-		}
-		else {
-
-			text.setString("Fail: Generic");
-			
-		}
-		this->wnd->draw(text);
-
-		Vector2f offset = Vector2f(5, 0);
-		for (int i = 0; i < this->current_text.getSize(); ++i) {
-			Text text2;
-			text2.setFont(g_Font);
-			text2.setCharacterSize(16);
-			text2.setFillColor(Color(200, 200, 200, 255));
-			text2.setString(this->current_text.substring(i, 1));
-
-			FloatRect bRect = text2.getGlobalBounds();
-			if (offset.x + bRect.width >= field_size.x) {
-				offset.y += 20;
-				offset.x = 5;
-			}
-
-			if (isErrored && i == expection.position) {
-				RectangleShape rect(Vector2f(5, 20));
-				rect.setPosition(field_pos + offset);
-				rect.setFillColor(Color(255, 0, 0, 255));
-				this->wnd->draw(rect);
-			}
-			text2.setPosition(field_pos + offset);
-			this->wnd->draw(text2);
-			offset.x += bRect.width + 0.5f;
-		}
-
-		if (isErrored && this->current_text.getSize() == expection.position) {
-			RectangleShape rect(Vector2f(5, 20));
-			rect.setPosition(field_pos + offset);
-			rect.setFillColor(Color(255, 0, 0, 255));
-			this->wnd->draw(rect);
-		}
 	}
 
 
 	void on_mousedown(Vector2f pos) {
-		Vector2f button_pos = Vector2f(250, 150);
-		Vector2f button_size = Vector2f(25, 25);
-		if (Rect<float>(button_pos, button_size).contains(pos)) {
-			this->button_state = !this->button_state;
-		}
+		for (Input* in : this->inputs)
+			in->on_mousedown(pos);
 	}
 
 	void on_mouseup(Vector2f pos) {
@@ -422,37 +536,119 @@ public:
 	}
 
 	void text_entered(Event evnt) {
-		if (evnt.text.unicode == 8) { // backspace
-			this->current_text = this->current_text.substring(0, this->current_text.getSize() - 1);
-		}
-		else if (evnt.text.unicode) {
-			this->current_text += evnt.text.unicode;
-		}
+		for (Input* in : this->inputs)
+			in->text_entered(evnt);
 	}
 };
+
+void Input::set_active(bool f) {
+	this->isActive = f;
+}
+
+Input::Input(Vector2f ps) {
+	this->pos = Vector2i(ps.x, ps.y);
+	this->size = Vector2i(200, 20);
+	this->set_active(0);
+}
+
+void Input::render(WndClass* wndcl) {
+	RectangleShape rect(Vector2f(this->size.x, this->size.y));
+	rect.setPosition(wndcl->wnd->mapPixelToCoords(pos));
+	rect.setOutlineColor(Color(0, 0, 0, 255));
+	rect.setOutlineThickness(1.f);
+	rect.setFillColor(Color(255, 255, 255, 255));
+	wndcl->wnd->draw(rect);
+
+	Text text;
+	text.setFont(g_Font);
+	text.setCharacterSize(16);
+	text.setFillColor(Color(0, 0, 0, 255));
+	// text.setScale(Vector2f(wndcl->zoomInternal, wndcl->zoomInternal));
+	if (this->value.getSize()) {
+		int cnt = this->value.getSize() - 1;
+		while (cnt >= 0 && text.getLocalBounds().width + 10 < size.x) {
+			text.setString(this->value[cnt--] + text.getString());
+		}
+		//this->lastDrawValue = text.getString();
+	}
+	Vector2f dp = Vector2f(this->pos.x, this->pos.y) + Vector2f(this->size.x - text.getLocalBounds().width, 0);
+	text.setPosition(wndcl->wnd->mapPixelToCoords(Vector2i(dp.x, dp.y)));
+	wndcl->wnd->draw(text);
+}
+
+void Input::rebuild() {
+	if (this->rootNode) {
+		delete this->rootNode;
+		this->rootNode = nullptr;
+	}
+	if (!this->value.getSize())
+		return;
+
+	try {
+		this->rootNode = ExprParse(Stream(this->value));
+	}
+	catch (...) {
+
+	}
+}
+
+void Input::text_entered(Event evnt) {
+	if (!this->isActive)
+		return;
+	if (evnt.text.unicode == 8) { // backspace
+		if (this->value.getSize())
+			this->value = this->value.substring(0, this->value.getSize() - 1);
+	}
+	else if (evnt.text.unicode) {
+		this->value += evnt.text.unicode;
+	}
+
+	this->rebuild();
+}
+
+void Input::on_mousedown(Vector2f pos) {
+	this->isActive = Rect<float>(this->pos.x, this->pos.y, this->size.x, this->size.y).contains(pos);
+}
 
 int main()
 {
 	REG_OPCODE(ConstNumber, "", 0, [](PNode node) -> double {
 		return node->arg;
-	})
+	});
 	REG_OPCODE(DynNumber, "x", 0, [](PNode node) -> double {
-		return node->arg;
-	})
-	REG_OPCODE(Plus, "+", 0, [](PNode node) -> double {
+		return g_GetDynVariable;
+	});
+	REG_OPCODE(Plus, "+", 1, [](PNode node) -> double {
 		return EvalNode(node->left) + EvalNode(node->right);
-	})
+	});
+	REG_OPCODE(Minus, "-", 1, [](PNode node) -> double {
+		return EvalNode(node->left) - EvalNode(node->right);
+	});
+	REG_OPCODE(Multiply, "*", 2, [](PNode node) -> double {
+		return EvalNode(node->left) * EvalNode(node->right);
+	});
+	REG_OPCODE(Divide, "/", 2, [](PNode node) -> double {
+		return EvalNode(node->left) / EvalNode(node->right);
+	});
+	REG_OPCODE(Pow, "^", 3, [](PNode node) -> double {
+		return pow(EvalNode(node->left), EvalNode(node->right));
+	});
+	REG_FUNC(Cos, "cos", [](PNode node) -> double {
+		return cos(EvalNode(node->right));
+	});
+	REG_FUNC(Sin, "sin", [](PNode node) -> double {
+		return sin(EvalNode(node->right));
+	});
 
 	String tests[] = {
-		String("x + 1"), // 1
-		
+		String("cos(x)"), // 635825
+
 	};
-	/*int num = 1;
+	int num = 1;
 	for (String s : tests) {
-		Parser1 parser(s);
 		try {
-			int rs = parser.parse();
-			cout << "Test " << num << ": OK (" << rs << ")" << endl;
+			PNode rs = ExprParse(s);
+			cout << "Test " << num << ": OK (" << EvalNode(rs) << ")" << endl;
 		}
 		catch (ReadExpection& ex) {
 			cout << "Test " << num << ": Fail (" << ex.what() << ")" << endl;
@@ -461,7 +657,7 @@ int main()
 			cout << "Test " << num << ": Fail (Generic)" << endl;
 		}
 		++num;
-	}*/
+	}
 
 	/*String tests[] = {
 		String("555+(0-(0-6))+(((0-6666)))+6106"), // 1
@@ -494,20 +690,39 @@ int main()
 
 	while (window.isOpen())
 	{
+		static bool _moving = false;
+		static Vector2f _start = Vector2f();
+
 		Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed) {
 				window.close();
 			}
-			else if (event.type == Event::MouseButtonPressed) {
+			else if (event.type == sf::Event::MouseButtonPressed) {
 				if (event.mouseButton.button == 0) {
+					_moving = true;
+					_start = Vector2f(Mouse::getPosition(window));
+
 					wnd->on_mousedown(Vector2f(event.mouseButton.x, event.mouseButton.y));
 				}
 			}
-			else if (event.type == Event::MouseButtonReleased) {
+			else if (event.type == sf::Event::MouseButtonReleased) {
 				if (event.mouseButton.button == 0) {
+					if (_moving) {
+						wnd->offset -= (Vector2f(Mouse::getPosition(window)) - _start);
+					}
+					_moving = false;
+
 					wnd->on_mouseup(Vector2f(event.mouseButton.x, event.mouseButton.y));
+				}
+			}
+			else if (event.type == sf::Event::MouseWheelScrolled) {
+				if (event.mouseWheelScroll.delta > 0) {
+					wnd->target_scale *= 1.07f;
+				}
+				else {
+					wnd->target_scale /= 1.05f;
 				}
 			}
 			else if (event.type == Event::TextEntered) {
@@ -516,8 +731,14 @@ int main()
 		}
 		window.clear(Color(30, 30, 30, 255));
 
+		Vector2f prev_offset = wnd->offset;
+		if (_moving) {
+			wnd->offset -= (Vector2f(Mouse::getPosition(window)) - _start);
+		}
+
 		wnd->frame();
 
+		wnd->offset = prev_offset;
 		window.display();
 	}
 
